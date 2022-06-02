80154ea35fd181a1f5cc30855f4c8f472322d6e5
peiyike <peiyike@vova.com.hk>
2022-06-02 14:47:33 +0800

    新增OC_Main

---
+++ main/OC_Main/OC_Main/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  OC_Main
+//
+//  Created by jianbo.pei on 2022/6/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
